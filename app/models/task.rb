e70d313051190daddb72c8507cea6911ba8c3205
yurikowada <komi3433@gmail.com>
2017-04-25 07:22:28 +0000

    login second

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,7 @@
 class Task < ApplicationRecord
   belongs_to :user
   
-  validates :content, presence: true, length: { maximum: 10 }
+  validates :content, presence: true
   validates :status, presence: true, length: { maximum: 10 }
   validates :user_id, presence: true
    
